e0ad952e6d4786b0d95e69f58e7f7bae53555c9d
Mark <mark@itaccomplice.co.uk>
2017-11-24 14:00:27 +0000

    First test fails

---
+++ src/com/itaccomplice/nhs/FizBuzz.java
@@ -0,0 +1,30 @@
+package com.itaccomplice.nhs;
+
+/**
+ * The FizBuzz class is a utility class that will convert a range of numbers into the specified
+ * FizBuzz output.
+ * @author Mark Ford
+ */
+public final class FizBuzz {
+
+	/**
+	 * Private constructor this is a utility class.
+	 */
+	private FizBuzz() {
+		// Empty private constructor to prevent instantiation.
+	}
+
+	/**
+	 * Convert the range of numbers into the specified FizBuzz output.
+	 * @param start Start number.
+	 * @param end End number.
+	 * @return The specified FizBuzz string output.
+	 */
+	public static String convert(int start, int end) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
--- test/com/itaccomplice/nhs/FizBuzzTest.java
+++ test/com/itaccomplice/nhs/FizBuzzTest.java
@@ -1,7 +1,12 @@
 package com.itaccomplice.nhs;
 
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
+/**
+ * Test FizBuzz complies with spec.
+ * @author Mark Ford
+ */
 public class FizBuzzTest {
 
 	/**
